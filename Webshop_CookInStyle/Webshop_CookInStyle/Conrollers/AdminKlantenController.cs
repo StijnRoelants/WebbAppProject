fe32afae73770a9c07e7f90dbb21a323ccd83cab
Stijn Roelants <r0792469@student.thomasmore.be>
2021-04-25 21:38:26 +0200

    KlantenbeheerAfgezerkt
    Module afgewerkt
    - Klanten kunnen aangepast worden
    - Aan een Klant kan een Admin Rol gekoppeld worden!

--- Webshop_CookInStyle/Webshop_CookInStyle/Conrollers/AdminKlantenController.cs
+++ Webshop_CookInStyle/Webshop_CookInStyle/Conrollers/AdminKlantenController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
@@ -14,6 +15,7 @@ using Webshop_CookInStyle.ViewModels;
 
 namespace Webshop_CookInStyle.Conrollers
 {
+    [Authorize(Roles ="Admin")]
     public class AdminKlantenController : Controller
     {
         private readonly WebshopContext _context;
@@ -124,6 +126,79 @@ namespace Webshop_CookInStyle.Conrollers
                 return NotFound();
             }
             return View(viewModel);
+        }
+
+        public async Task<IActionResult> EditKlant(string id)
+        {
+            EditKlantVM viewModel = new EditKlantVM();
+            if (id == null)
+            {
+                NotFound();
+            }
+            viewModel.UserRoleList = _context.UserRoles;
+            viewModel.Klant = await _context.Klanten.Where(x => x.Id == id).Include(x => x.Postcode).Include(x => x.Land).FirstOrDefaultAsync();
+            viewModel.Postcodes = new SelectList(_context.Postcodes.OrderBy(x => x.Nummer), "PostcodeID", "Weergave");
+            viewModel.Landen = new SelectList(_context.Landen.OrderBy(x => x.Naam), "LandID", "Naam");
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditKlant(string id, EditKlantVM viewModel)
+        {
+            if (id != viewModel.Klant.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                Klant aangepasteKlant = await _context.Klanten
+                    .Include(x => x.Postcode)
+                    .Include(x => x.Land)
+                    .Where(x => x.Id == id)
+                    .FirstOrDefaultAsync();
+
+                if (viewModel.Klant.Email.ToUpper() != aangepasteKlant.Email.ToUpper())
+                {
+                    aangepasteKlant.Email = viewModel.Klant.Email.ToLower();
+                    aangepasteKlant.NormalizedEmail = viewModel.Klant.Email.ToUpper();
+                    aangepasteKlant.NormalizedUserName = viewModel.Klant.Email.ToUpper();
+                    aangepasteKlant.UserName = viewModel.Klant.Email.ToLower();
+                }
+
+                if (viewModel.MaakAdmin == true)
+                {
+                    DbSet<IdentityUserRole<string>> roles = _context.UserRoles;
+                    IdentityRole adminRole = _context.Roles.Where(x => x.NormalizedName == "ADMIN").FirstOrDefault();
+                    if (adminRole != null)
+                    {
+                        if (!roles.Any(x => x.UserId == aangepasteKlant.Id && x.RoleId == adminRole.Id))
+                        {
+                            roles.Add(new IdentityUserRole<string>() { UserId = aangepasteKlant.Id, RoleId = adminRole.Id });
+                        }
+                    }
+                }
+
+                if (viewModel.Klant.IsBedrijf == true)
+                {
+                    aangepasteKlant.Naam = viewModel.Klant.Naam;
+                    aangepasteKlant.BtwNummer = viewModel.Klant.BtwNummer;
+                }
+
+                aangepasteKlant.Voornaam = viewModel.Klant.Voornaam;
+                aangepasteKlant.Achternaam = viewModel.Klant.Achternaam;
+                aangepasteKlant.StraatEnNummer = viewModel.Klant.StraatEnNummer;
+                aangepasteKlant.Telefoon = viewModel.Klant.Telefoon;
+                aangepasteKlant.Mobiel = viewModel.Klant.Mobiel;
+                aangepasteKlant.PostcodeID = viewModel.Klant.PostcodeID;
+                aangepasteKlant.LandID = viewModel.Klant.LandID;
+
+                _context.Update(aangepasteKlant);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View("Index", viewModel);
         }
     }
     }
---
+++ Webshop_CookInStyle/Webshop_CookInStyle/ViewModels/EditKlantVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Webshop_CookInStyle.Models;
+
+namespace Webshop_CookInStyle.ViewModels
+{
+    public class EditKlantVM
+    {
+        public Klant Klant { get; set; }
+        public SelectList Postcodes { get; set; }
+        public SelectList Landen { get; set; }
+        public bool MaakAdmin { get; set; }
+        public DbSet<IdentityUserRole<string>> UserRoleList { get; set; }
+    }
+}
