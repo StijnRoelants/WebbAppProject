fe8b5154031b89d903b1904e0ab65c824f3b8449
StijnRoelants <stijn.roelants@gmail.com>
2021-05-23 15:01:10 +0200

    uow En Api Tests

---
+++ Webshop_CookInStyle/Webshop_CookInStyle/Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webshop_CookInStyle.Data.Repository;
+using Webshop_CookInStyle.Models;
+
+namespace Webshop_CookInStyle.Data.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<Bestelling> BestellingRepository { get; }
+        IGenericRepository<Bestellijn> BestellijnRepository { get; }
+        IGenericRepository<Product> ProductRepository { get; }
+        Task Save();
+    }
+}
--- Webshop_CookInStyle/Webshop_CookInStyle/Startup.cs
+++ Webshop_CookInStyle/Webshop_CookInStyle/Startup.cs
@@ -6,11 +6,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Webshop_CookInStyle.Data;
+using Webshop_CookInStyle.Data.Repository;
+using Webshop_CookInStyle.Data.UnitOfWork;
+using Webshop_CookInStyle.Helpers;
 using Webshop_CookInStyle.Models;
 
 namespace Webshop_CookInStyle
@@ -29,8 +35,9 @@ namespace Webshop_CookInStyle
         {
             //services.AddRazorPages();
 
-            services.AddControllersWithViews();
-
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+                
             services.AddDbContext<WebshopContext>(options
                 => options.UseSqlServer(Configuration.GetConnectionString("WebshopConnection")));
 
@@ -38,7 +45,66 @@ namespace Webshop_CookInStyle
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<WebshopContext>();
 
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            // JWT Configuratie
+            var appSettings = Configuration.GetSection("AppSettings").Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication()
+                .AddJwtBearer(x =>
+               {
+                   x.RequireHttpsMetadata = false;
+                   x.SaveToken = true;
+                   x.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuerSigningKey = true,
+                       IssuerSigningKey = new SymmetricSecurityKey(key),
+                       ValidateIssuer = false,
+                       ValidateAudience = false
+               };
+               });
+
+            // Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("V1", new OpenApiInfo { Title = "My API", Version = "V1" });
+
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[0] }
+                };
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme.",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                    }
+                });
+            });
+
+            #region IGenerics & UOW
+            services.AddScoped<IGenericRepository<Bestelling>, GenericRepository<Bestelling>>();
+            services.AddScoped<IGenericRepository<Bestellijn>, GenericRepository<Bestellijn>>();
+            services.AddScoped<IGenericRepository<Product>, GenericRepository<Product>>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            #endregion
+
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            #region Identity 
             services.Configure<IdentityOptions>(options =>
             {
                 options.Password.RequireDigit = true;
@@ -58,6 +124,7 @@ namespace Webshop_CookInStyle
                 "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
                 options.User.RequireUniqueEmail = false;
             });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -77,6 +144,13 @@ namespace Webshop_CookInStyle
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseRouting();
 
             app.UseAuthentication();
@@ -94,6 +168,7 @@ namespace Webshop_CookInStyle
             //CreateUserRoles(serviceProvider).Wait();
         }
 
+        #region Create Roles
         private async Task CreateUserRoles( IServiceProvider serviceProvider)
         {
             RoleManager<IdentityRole> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
@@ -123,5 +198,6 @@ namespace Webshop_CookInStyle
                 }
             }
         }
+        #endregion
     }
 }
